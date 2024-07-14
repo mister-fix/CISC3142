9386b2e5cbdf32675b1d6abff63b4fb3cf6f7b93
Stephen <hellostephenwm@gmail.com>
2024-07-14 17:56:11 -0400

    moved files to subdirectory 1-1

---
+++ Assignments/01/1-1/app.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "+----------------------+" << endl;
+    cout << "| Hello from CISC 3142 |" << endl;
+    cout << "+----------------------+" << endl;
+    return 0;
+}
